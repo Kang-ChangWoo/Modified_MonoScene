860f24c9bdc2eb76eeb7df30f88942a2a5e3be5d
changwoo <branden.c.w.kang@gmail.com>
2022-07-07 11:12:33 +0000

    update

---
+++ train.sh
@@ -0,0 +1,7 @@
+python monoscene/scripts/train_monoscene.py \
+    dataset=NYU \
+    NYU_root=$NYU_ROOT \
+    NYU_preprocess_root=$NYU_PREPROCESS \
+    logdir=$NYU_LOG \
+    n_gpus=2 \
+    batch_size=2
